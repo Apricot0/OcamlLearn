cf4f12d5d40de673536f7a8f558594cb22774102
Jasper Zeng <61392940+Apricot0@users.noreply.github.com>
2022-09-23 20:22:00 +0000

    pow add

--- bin/main.ml
+++ bin/main.ml
@@ -1,20 +1,8 @@
+let rec pow x n = 
+ if n = 0 then 1
+ else x*pow x (n-1);;
 
-type task = {
-  name: string;
-  priority: int;
-}
-
-let make_task name priority = {name; priority}
-
-let t0 = make_task "📿" 0
-
-let () = print_endline "***********"
-let () = print_endline "|...$$$...|"
-let () = print_endline "|---$$$---|"
-let () = print_endline "|...$$$...|"
-let () = print_endline "***********\n\n"
-
-let () = print_endline "available symbols: t0, make_task"
-let () = print_endline "add ;; after each expression (make_task;; || t0;; ...) "
-
+let rec float_pow x n = 
+ if n = 0 then 1
+ else x*.pow x (n-1);;
 
