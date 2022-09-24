7e583ccec4977ac7d94ef3323c2136d8fc0100aa
Jasper Zeng <61392940+Apricot0@users.noreply.github.com>
2022-09-24 00:37:40 +0000

    update Q2

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,19 @@
+# Q1
 let rec pow x n = 
  if n = 0 then 1
  else x*pow x (n-1);;
 
 let rec float_pow x n = 
- if n = 0 then 1
- else x*.pow x (n-1);;
+ if n = 0 then 1.0
+ else x *. float_pow x (n-1);;
+
+# Q2
+let rec compress list = match list with
+  | [] -> []
+  | a::[] -> [a]
+  | x::y::t -> 
+      if x = y then compress (x::t) 
+      else x::compress (y::t);;
+#Q3
+
 
